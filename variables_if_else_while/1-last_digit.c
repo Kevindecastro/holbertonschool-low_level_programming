4db133e46caede7b9deee667711aacb46cfa2776
Kevindecastro <9830@holbertonstudents.com>
2024-10-22 22:09:54 +0000

    add 1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
