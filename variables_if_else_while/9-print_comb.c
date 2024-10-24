00147040d2d0f1af4d8a7234ba83a4b8e2423c2d
Kevindecastro <9830@holbertonstudents.com>
2024-10-24 12:01:00 +0000

    add 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 int number;
 
-for (number = 0; number < 10; number++)
+for (number = 0; number <= 9; number++)
 {
 putchar(number + '0');
-if (number < 10)
+if (number != 9)
 {
 putchar(',');
 }
