9f48be562c94147aefc4bfb0bb72f3b6de9b1240
Kevindecastro <9830@holbertonstudents.com>
2024-10-28 14:52:30 +0000

    add 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /*
- * print_most_numbers - Print the Alphabet and ignore 2 and 4.
- * Description: Prints the numbers excluding 2 and 4
+ * print_most_numbers - Print the Alphabet and ignore 2 and 4
  * Return: The numbers since 0 up to 9
  */
 
-void print_most_numbers (void)
+void print_most_numbers(void)
 {
 	int num = 0;
+
 	for (num = 48; num <= 57; num++)
 	{
 		if (num != 50 && num != 52)
