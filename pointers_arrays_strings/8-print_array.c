8aa393e071fef198b7e39fe9d6e547ac1bdd1702
Kevindecastro <9830@holbertonstudents.com>
2024-11-03 11:23:24 +0000

    patch main.h + patch 8-print_array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - Entry Function
@@ -14,10 +15,10 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
+	}
+	if (i < n - 1)
+	{
+		printf(", ");
 	}
 	printf("\n");
 }
