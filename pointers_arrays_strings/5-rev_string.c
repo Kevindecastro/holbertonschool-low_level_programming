82e9136c3230b235e3257f4520bf71faf6d53d94
Kevindecastro <9830@holbertonstudents.com>
2024-11-03 10:33:43 +0000

    add 5-main.c + add 5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* rev_string - Entry Function
+* @s: reset The pointers
+* Return: returns nothing
+*/
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
+	}
+}
