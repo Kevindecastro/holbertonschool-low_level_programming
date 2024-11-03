d912e5db090ed379a6944597e38539f7f733ad8d
Kevindecastro <9830@holbertonstudents.com>
2024-11-03 11:39:21 +0000

    patch 8-print_array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,27 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * print_array - Entry Function
- * @a: reset The pointers
- * @n: Print the pointers
- * Return: returns nothing
+ * print_array - print an array
+ * @a: integer, array
+ * @n: integer number of index
+ * Return: void
  */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
+		printf("\n");
+	else
 	{
-		printf("%d", a[i]);
+		for (i = 0; i < n; i++)
+		{
+			if (i != n - 1)
+				printf("%d, ", a[i]);
+			else
+				printf("%d\n", a[i]);
+		}
 	}
-	if (i < n - 1)
-	{
-		printf(", ");
-	}
-	printf("\n");
 }
