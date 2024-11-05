e44f60f6b5a9ee27315f43e25de7c25f8bea8ca0
Kevindecastro <9830@holbertonstudents.com>
2024-11-05 08:51:36 +0000

    patch 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,13 @@
+#include <stdio.h>
 #include "main.h"
-#include "2-strchr.c"
+
 /**
  * _strpbrk - searches a string for any of sat of bytes
  * @s: string sources
  * @accept: string dest
  * Return: s if match with bytes of accept, else return null
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 	char *p = s;
