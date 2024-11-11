2fef9cc784c97ba67c8b01b718ac4c842b993d25
Kevindecastro <9830@holbertonstudents.com>
2024-11-11 11:44:49 +0000

    add main.h + add README.md + add _putchar.c + add 0-main.c + add + add 0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create an array of char
+ * @size: unsigned int for size
+ * @c: char initialize
+ * Return: Null if size 0, else return size
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	array = malloc(size);
+	if (size == 0)
+		return (NULL);
+	if (array == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
