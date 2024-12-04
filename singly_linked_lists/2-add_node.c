62b980bade8283ea9ee0ad947858fb5361233c8b
Kevindecastro <9830@holbertonstudents.com>
2024-12-04 09:11:10 +0000

    correctis synthax for betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,30 +4,38 @@
 #include "lists.h"
 
 /**
- * add_node - Ajoute un nouveau noeud au dÃ©but d'une liste chaÃÃ
- * @head: Pointeur vers le pointeur de la tÃªte de la list
- * @str: Chai®ne de carace¨e a dupliquer et a inse©rer dans lenoeud
+ * add_node - Adds a new node to the beginning of a chain list
+ * @head: Pointer to the pointer of the head of the list
+ * @str: String to duplicate and insert into the node
  *
- * Return: Adresse du nouveau noeud ou NULL en cas d'Ã©che
+ * Return: Address of the new node or NULL on failure
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+	list_t *new_node;
+	unsigned int len = 0;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	while (str[len])
+		len++;
 
-    new_node->next = *head;
-    *head = new_node;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    return (new_node);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
