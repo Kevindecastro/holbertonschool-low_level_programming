3640f68e5ded24527effaa85950476962a1da640
Kevindecastro <9830@holbertonstudents.com>
2024-12-04 12:30:15 +0000

    add the prototype : void free_list(list_t *head) for free the memory

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* free_list - Function to free the linked list
+* @head: pointers of the link
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+	list_t *next;
+	temp = head;
+	while (temp != NULL)
+	{
+		next = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = next;
+	}
+}
