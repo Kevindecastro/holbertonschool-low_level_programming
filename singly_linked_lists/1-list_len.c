1548e37d548b24ad708c45b750fb13685938372b
Kevindecastro <9830@holbertonstudents.com>
2024-12-04 09:07:28 +0000

    correcting a space for betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,18 +8,18 @@
 * list_len - Function for show the list
 * @h: pointers contains string
 *
-* Return: Number Elements
+* Return: Returns the length of the list
 */
 
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
 
-    return count;
+	return (count);
 }
