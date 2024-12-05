01520bf113d7ddb40a82f66c1a39add01eb94cf0
Kevindecastro <9830@holbertonstudents.com>
2024-12-05 09:12:23 +0000

    correctif 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,18 +9,14 @@
 *
 * Return: number of nodes
 */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	const dlistint_t *current = h;
-
-	while (current != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", current->n);
+		printf("%d\n", h->n);
 		count++;
-		current = current->next;
+		h = h->next;
 	}
-
 	return (count);
 }
