d1e2c682352570fa591b52dd3a1d205fb333c253
Kevindecastro <9830@holbertonstudents.com>
2024-12-07 15:22:06 +0000

    patch synthax for betty file 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,36 +3,37 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Ajoute du texte à la fin d'un fichier.
- * @filename: Nom du fichier.
- * @text_content: Texte à ajouter.
+ * append_text_to_file - Appends text to the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The text to append.
  *
- * Return: 1 en cas de succès, -1 en cas d'échec.
+ * Return: 1 on success, -1 on failure.
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes_written;
 	size_t length = 0;
 
-	/*Vérifie si filename est NUL */
+	/*Check if filename is NULL */
 	if (filename == NULL)
 		return (-1);
 
-	/*Ouvre le fichier en mode écriture et ajoute à la fin (O_APPEND) */
+	/*Open the file in write mode and append to the end (O_APPEND) */
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)/*Si ouverture échoue, retourne - */
+	if (fd == -1)/*If opening the file fails, return -1 */
 		return (-1);
 
-	/*Si text_content n'est NULL, calcule sa longueur et l'écrit dans le fichie */
+	/*If text_content is not NULL, calculate its len and WR it to the file */
 	if (text_content != NULL)
 	{
 		while (text_content[length])
 			length++;
 
-		/*écrit le texte à la fin du fich*/
+		/*Write the text to the end of the file*/
 		bytes_written = write(fd, text_content, length);
-		if (bytes_written == -1) /*Si l'écriture échoue, retou*/
+		if (bytes_written == -1) /*If writing fails, return -1*/
 		{
 			close(fd);
 			return (-1);
