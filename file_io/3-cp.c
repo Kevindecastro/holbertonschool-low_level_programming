e1af88ee220cd850f5d02a6bd9aaff661380b5e6
Kevindecastro <9830@holbertonstudents.com>
2024-12-07 17:24:33 +0000

    patch 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,59 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
 #include "main.h"
 
-#define BUF_SIZE 1024
+#define BUFFER_SIZE 1024
 
 /**
- * main - Copie le contenu d'un fichier dans un autre.
- * @argc: Le nombre d'arguments.
- * @argv: Les arguments.
+ * main - Copies the content of a file to another file.
+ * @argc: Argument count.
+ * @argv: Argument vector.
  *
- * Return: Toujours 0.
+ * Return: 0 on success, exits with appropriate codes on failure.
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, bytes_read, bytes_written;
-	char buffer[BUF_SIZE];
-
+	int file_from, file_to, read_bytes, write_bytes;
+	char buffer[BUFFER_SIZE];
+	/* Check if the number of arguments is correct */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
+	/* Open the source file for reading */
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
+	/* Open the destination file for writing, create it if necessary */
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	while ((bytes_read = read(file_from, buffer, BUF_SIZE)) > 0)
+	/* Read and write data in chunks */
+	while ((read_bytes = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+		write_bytes = write(file_to, buffer, read_bytes);
+		if (write_bytes == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-
-	if (bytes_read == -1)
+	/* Check for read error */
+	if (read_bytes == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
+	/* Close the file descriptors */
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
