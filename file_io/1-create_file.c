d56dd88561d85a5def9e0a9a14c76767fa2156f2
Kevindecastro <9830@holbertonstudents.com>
2024-12-07 14:46:48 +0000

    patch synthax for betty file 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* Open file in write-only mode, create it if it doesn't exist, and truncate */
+	/* Open file in write-only mode, create it if doesn't exist and truncate*/
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
