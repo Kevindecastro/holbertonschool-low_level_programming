e45131fc2d3399b04828eb3458592728f55a878e
Kevindecastro <9830@holbertonstudents.com>
2024-12-08 10:37:04 +0000

    correctif 3-cp.c and main.h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,97 +1,99 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- * handle_error - Prints an error message and exits with the corresponding code.
- * @code: Error code to exit with.
- * @arg: Additional argument for error message formatting (if needed).
+ * print_error - Print error message and exit program
+ * @message: Custom error message
+ * @file: File causing the error (or NULL if irrelevant)
+ * @exit_code: Exit code to terminate the program
  */
-void handle_error(int code, const char *arg)
+void print_error(const char *message, const char *file, int exit_code)
 {
-	switch (code)
-	{
-		case 97:
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			break;
-		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
-			break;
-		case 99:
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg);
-			break;
-		case 100:
-			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", arg);
-			break;
-		default:
-			dprintf(STDERR_FILENO, "Unknown error\n");
-			break;
-	}
-	exit(code);
+	if (file)
+		dprintf(STDERR_FILENO, "%s %s\n", message, file);
+	else
+		dprintf(STDERR_FILENO, "%s\n", message);
+	exit(exit_code);
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @argc: Argument count.
- * @argv: Argument vector.
+ * close_file - Close a file descriptor with error handling
+ * @fd: File descriptor to close
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * copy_file - Copy content from one file to another
+ * @file_from: Path to the source file
+ * @file_to: Path to the destination file
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buf[BUFFER_SIZE];
+
+	/* Open source file */
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		print_error("Error: Can't read from file", file_from, 98);
+	/* Open/create destination file */
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close_file(fd_from);
+		print_error("Error: Can't write to file", file_to, 99);
+	}
+	/* Read and write loop */
+	while ((bytes_read = read(fd_from, buf, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buf, bytes_read);
+		if (bytes_written == -1 || bytes_written < bytes_read)
+		{
+			close_file(fd_from);
+			close_file(fd_to);
+			print_error("Error: Can't write to file", file_to, 99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		close_file(fd_from);
+		close_file(fd_to);
+		print_error("Error: Can't read from file", file_from, 98);
+	}
+
+	/* Close both files */
+	close_file(fd_from);
+	close_file(fd_to);
+}
+
+/**
+ * main - Entry point for the program
+ * @argc: Number of arguments
+ * @argv: List of arguments
  *
- * Return: 0 on success, exits with appropriate codes on failure.
+ * Return: 0 on success, error codes otherwise
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	ssize_t read_bytes, write_bytes;
-	char buffer[BUFFER_SIZE];
-	char fd_str[12];
-	/*buffer to store file descriptor as str for error reporting*/
 	if (argc != 3)
-		handle_error(97, NULL);
-	/* Open the source file for reading */
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-		handle_error(98, argv[1]);
-	/* Open the destination file for writing, create it if necessary */
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
-	{
-		close(file_from); /* Ensure the source file is closed before exiting */
-		handle_error(99, argv[2]);
-	}
-	/* Read and write data in chunks */
-	while ((read_bytes = read(file_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		write_bytes = write(file_to, buffer, read_bytes);
-		if (write_bytes == -1)
-		{
-			close(file_from);
-			close(file_to);
-			handle_error(99, argv[2]);
-		}
-	}
-	/* Check for read error */
-	if (read_bytes == -1)
-	{
-		close(file_from);
-		close(file_to);
-		handle_error(98, argv[1]);
-	}
-	/* Close the file descriptors */
-	if (close(file_from) == -1)
-	{
-		snprintf(fd_str, sizeof(fd_str), "%d", file_from);
-		handle_error(100, fd_str);
-	}
+		print_error("Usage: cp file_from file_to", NULL, 97);
 
-	if (close(file_to) == -1)
-	{
-		snprintf(fd_str, sizeof(fd_str), "%d", file_to);
-		handle_error(100, fd_str);
-	}
+	copy_file(argv[1], argv[2]);
 
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void print_error(const char *message, const char *file, int exit_code);
+void close_file(int fd);
+void copy_file(const char *file_from, const char *file_to);
 
 #endif
